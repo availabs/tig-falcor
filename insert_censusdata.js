a50fe5a0089c53aa63782f83eff0aa09f12c7190
sarangp3010 <spatel25@albany.edu>
2023-07-20 14:03:21 -0400

    added script to load tracts into database

---
+++ insert_censusdata.js
@@ -0,0 +1,84 @@
+var { get } = require("lodash");
+
+const falcorGraph = require("./test/graph");
+
+const chunkSize = 1;
+const geoids = [36, 34, "09"];
+const years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019];
+const censusKeys = [
+  "B02001_001E",
+  "B02001_004E",
+  "B02001_005E",
+  "B02001_003E",
+  "B02001_006E",
+  "B02001_007E",
+  "B02001_008E",
+  "B02001_002E"
+];
+
+const chunkArray = (array, size) => {
+  const chunkedArray = [];
+  for (let i = 0; i < array.length; i += size) {
+    const chunk = array.slice(i, i + size);
+    chunkedArray.push(chunk);
+  }
+  return chunkedArray;
+};
+
+const geoidsChunks = chunkArray(geoids, chunkSize);
+const processChunks = async (geoidsChunks, years, censusKeys) => {
+  let currentChunk = 0;
+
+  for (let geoids of geoidsChunks) {
+    console.log(
+      `\n\n\n  ------------- Started Chunk: ${currentChunk}: ---------- \n\n\n`
+    );
+
+    const getCountiesEvent = {
+      paths: [["geo", geoids.map(String), "counties"]],
+      method: "get"
+    };
+    const getTractsEvent = {
+      paths: [["geo", geoids.map(String), "tracts"]],
+      method: "get"
+    };
+
+    const t = await falcorGraph.respond({
+      queryStringParameters: getCountiesEvent
+    });
+    const v = await falcorGraph.respond({
+      queryStringParameters: getTractsEvent
+    });
+
+    let geo = [
+      ...(geoids || []).reduce((a, c) => {
+        a = [
+          ...a,
+          ...get(t, ["jsonGraph", "geo", c, "counties", "value"], []),
+          ...get(v, ["jsonGraph", "geo", c, "tracts", "value"], [])
+        ];
+        return a;
+      }, new Set())
+    ];
+
+    console.log("geo", geo, geoids);
+    const getEvent = {
+      paths: [["acs", geo, years, censusKeys]],
+      method: "get"
+    };
+
+    await falcorGraph.respond({ queryStringParameters: getEvent });
+    console.log(
+      `\n\n\n  ------------- Ended Chunk: ${currentChunk}: ---------- \n\n\n`
+    );
+    currentChunk++;
+  }
+};
+
+processChunks(geoidsChunks, years, censusKeys)
+  .then(() => {
+    console.time("Completed");
+  })
+  .catch(err => {
+    console.error("Error occurred during processing:", err);
+  });
